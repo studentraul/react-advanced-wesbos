33ccd86d352856708f2e049e587fb3882de184d5
Raul Melo <raulmelo@Rauls-MacBook-Pro.local>
2018-11-05 09:17:20 +0100

    Add NProgress

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,8 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
+import Router from "next/router";
+import NProgress from "nprogress";
 
 import Nav from "./Nav";
+
+/* Listening to Router Events */
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -47,7 +58,9 @@ const Header = () => {
     <StyledHeader>
       <div className="bar">
         <Logo>
-          <Link href="/">Sick Fits</Link>
+          <Link href="/">
+            <a>Sick Fits</a>
+          </Link>
         </Logo>
         <Nav />
       </div>
