cf7eaed1a0089fbdfb0759ab954de79b89d7b9a9
Raul Melo <raulmelo@Rauls-MacBook-Pro.local>
2018-11-06 05:27:53 +0100

    Create Mutation and Query related to icon

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,20 @@
-const mutations = {};
+// Push data
+const Mutations = {
+  async createItem(parent, args, context, info) {
+    // TODO: Check if they are logged in
 
-module.exports = mutations;
+    // Access db mutation (prisma.graphql)
+    const item = await context.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info // The actual query
+    );
+
+    return item;
+  }
+};
+
+module.exports = Mutations;
