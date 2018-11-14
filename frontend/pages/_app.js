ca76ffc9b72cf27703b478deb4d09d9515cd9567
Raul Melo <raulmelo@Rauls-MacBook-Pro.local>
2018-11-14 15:44:10 +0100

    Setup Apollo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,15 +1,35 @@
 import App, { Container } from "next/app";
-import Page from "../components/Page";
+import { ApolloProvider } from "react-apollo";
 
-export default class CustomApp extends App {
+import Page from "../components/Page";
+import withData from "../lib/withData";
+
+class CustomApp extends App {
+  // Particularity from next to get "page=1... page=2... etc"
+  static async getInitialProps({ Component, context }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(context);
+    }
+    // This exposes query to the user
+    // pageProps.query = context.query; //not working =S
+    // console.log("VAI SE FODER", context);
+    
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
+
+export default withData(CustomApp);
